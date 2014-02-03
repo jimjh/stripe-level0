10dcc301011cb56caa88c3915eadf549e0b5d90e
Jim Lim <jim@quixey.com>
2014-02-02 19:21:01 -0800

    java does not seem to be much faster

---
+++ SpellCheck.java
@@ -0,0 +1,45 @@
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.*;
+import java.io.*;
+
+public class SpellCheck {
+
+  private static final String PATH = "/usr/share/dict/words";
+
+  public static void main(String[] args) {
+
+    String path = (args.length >= 1) ? args[0] : PATH;
+
+    Set<String> words = new HashSet<>();
+    Path p = Paths.get(PATH);
+    try(BufferedReader reader = Files.newBufferedReader(p, StandardCharsets.UTF_8)) {
+      String line;
+      while((line = reader.readLine()) != null) words.add(line.trim());
+    } catch (IOException e) {
+      System.err.println("caught exception: " + e.getMessage());
+      e.printStackTrace(System.err);
+    }
+
+    try(Scanner scanner = new Scanner(System.in, "UTF-8")) {
+      while(scanner.hasNextLine()) {
+        String line = scanner.nextLine();
+        String[] parts = line.split("\\s");
+        for (int i=0; i<parts.length; i++) {
+          String word = parts[i];
+          if (words.contains(word.toLowerCase())) {
+            System.out.print(word);
+          } else {
+            System.out.print("<" + word + ">");
+          }
+          if (i != parts.length - 1) System.out.print(" ");
+        }
+        System.out.println();
+      }
+    }
+
+  }
+
+}
