e29b9ca53b881804701ae9eb47ec5eddfea00e2d
Jim Lim <jim@quixey.com>
2014-02-02 23:03:20 -0800

    using mmap is even slower

--- src/main/java/jim/SpellCheck.java
+++ src/main/java/jim/SpellCheck.java
@@ -1,11 +1,17 @@
 package jim;
 
-import java.util.Set;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.nio.CharBuffer;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.HashSet;
 import java.util.Scanner;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.*;
-import java.io.*;
+import java.util.Set;
 
 public class SpellCheck {
 
@@ -13,26 +19,33 @@ public class SpellCheck {
 
   private static Set<String> readDict(String path) throws IOException {
     Set<String> words = new HashSet<>();
-    Path p = Paths.get(path);
-    try(BufferedReader reader = Files.newBufferedReader(p, StandardCharsets.UTF_8)) {
-      String line;
-      while((line = reader.readLine()) != null) words.add(line.trim());
-    }
+
+    FileChannel fch = FileChannel.open(Paths.get(path), StandardOpenOption.READ);
+    MappedByteBuffer mbb = fch.map(FileChannel.MapMode.READ_ONLY, 0L, fch.size());
+
+    // while(mbb.hasRemaining()) {
+      CharBuffer buffer = StandardCharsets.UTF_8.decode(mbb);
+      try(Scanner scanner = new Scanner(buffer)) {
+        while(scanner.hasNext()) words.add(scanner.next());
+      }
+    // }
+
+    fch.close();
     return words;
   }
 
   private static void filterInput(Set<String> words, InputStream input, PrintStream output) {
-    try(Scanner scanner = new Scanner(input, "UTF-8")) {
+    try (Scanner scanner = new Scanner(input, "UTF-8")) {
       scanner.useDelimiter("(?<=\\s)");
-      while(scanner.hasNext()) {
+      while (scanner.hasNext()) {
         String match = scanner.next();
-        String word  = match.substring(0, match.length() - 1);
+        String word = match.substring(0, match.length() - 1);
         if (words.contains(word.toLowerCase())) {
           output.print(word);
         } else {
           output.print("<" + word + ">");
         }
-        output.print(match.charAt(match.length()-1));
+        output.print(match.charAt(match.length() - 1));
       }
     }
   }
@@ -42,5 +55,4 @@ public class SpellCheck {
     Set<String> words = readDict(path);
     filterInput(words, System.in, System.out);
   }
-
 }
