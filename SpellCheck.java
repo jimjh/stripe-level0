1a0ff9e75f8610c084f8bac8cc9050554025e1b1
Jim Lim <jim@quixey.com>
2014-02-02 19:47:39 -0800

    using clever regex is even slower

--- SpellCheck.java
+++ SpellCheck.java
@@ -1,6 +1,8 @@
 import java.util.Set;
 import java.util.HashSet;
 import java.util.Scanner;
+import java.util.regex.MatchResult;
+import java.util.regex.Pattern;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.*;
 import java.io.*;
@@ -23,21 +25,34 @@ public class SpellCheck {
       e.printStackTrace(System.err);
     }
 
+    Pattern pattern = Pattern.compile("(\\S+)(\\s+)");
     try(Scanner scanner = new Scanner(System.in, "UTF-8")) {
-      while(scanner.hasNextLine()) {
-        String line = scanner.nextLine();
-        String[] parts = line.split("\\s");
-        for (int i=0; i<parts.length; i++) {
-          String word = parts[i];
-          if (words.contains(word.toLowerCase())) {
-            System.out.print(word);
-          } else {
-            System.out.print("<" + word + ">");
-          }
-          if (i != parts.length - 1) System.out.print(" ");
+      scanner.useDelimiter("(?<=\\s)");
+      while(scanner.hasNext(pattern)) {
+        scanner.next(pattern);
+        MatchResult match = scanner.match();
+        String word = match.group(1);
+        if (words.contains(word.toLowerCase())) {
+          System.out.print(word);
+        } else {
+          System.out.print("<" + word + ">");
         }
-        System.out.println();
+        System.out.print(match.group(2));
       }
+      //while(scanner.hasNextLine()) {
+      //  String line = scanner.nextLine();
+      //  String[] parts = line.split("\\s");
+      //  for (int i=0; i<parts.length; i++) {
+      //    String word = parts[i];
+      //    if (words.contains(word.toLowerCase())) {
+      //      System.out.print(word);
+      //    } else {
+      //      System.out.print("<" + word + ">");
+      //    }
+      //    if (i != parts.length - 1) System.out.print(" ");
+      //  }
+      //  System.out.println();
+      //}
     }
 
   }
